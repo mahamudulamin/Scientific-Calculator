269d70613057cc82404d8d917bb613dd7e1f3508
arif-bepari <137918398+arif-bepari@users.noreply.github.com>
2024-01-06 11:38:49 +0600

    display design

--- SC code .cpp
+++ SC code .cpp
@@ -1,1 +1,18 @@
+cout<<"\n\t-------------------------------"<<endl;
+    cout<<"\t           CALCULATOR            "<<endl;
+    cout<<"\t-------------------------------"<<endl;
 
+    cout<<"\t 1: Addition\t\t"<<" 9: Sine"<<endl;
+    cout<<"\t 2: Subtraction\t\t"<<"10: Cosine"<<endl;
+    cout<<"\t 3: Multiplication\t"<<"11: Tangent"<<endl;
+    cout<<"\t 4: Division\t\t"<<"12: Inverse of Sine"<<endl;
+    cout<<"\t 5: Exponentiation\t"<<"13: Inverse of Cosine"<<endl;
+    cout<<"\t 6: Logarithm\t\t"<<"14: Inverse of Tangent"<<endl;
+    cout<<"\t 7: Square\t\t"<<"15: Factorial"<<endl;
+    cout<<"\t 8: Square Root\t\t"<<"16: Exist"<<endl;
+
+    cout<<"\n\t Special Modes:"<<endl;
+    cout<<" \t --------------"<<endl;
+    cout<<"\t 17: Calculate an Expression"<<endl;
+    cout<<"\t 18. Matrix Operations"<<endl;
+    cout<<"\t 19. Vector Operations"<<endl;
